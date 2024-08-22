fb8569e9077d68ebd58afdd26ec623604a769126
Manoj Kamatam <128217010+ManojKamatam@users.noreply.github.com>
2024-08-22 20:59:19 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,3 @@
-
 provider "aws" {
   region = "us-east-1"
 }
@@ -20,14 +19,15 @@ resource "aws_elastic_beanstalk_application" "my_app" {
 
 resource "aws_elastic_beanstalk_application_version" "my_app_version" {
   application = aws_elastic_beanstalk_application.my_app.name
-  version_label = "v1.0.0"
-  s3_bucket = var.s3_bucket
-  s3_key = var.s3_key
+  name        = "my-app-version"
+  description = "My Elastic Beanstalk Application Version"
+  bucket      = var.s3_bucket
+  key         = var.s3_key
 }
 
 resource "aws_elastic_beanstalk_environment" "my_env" {
-  name                = "Sample-beanstalk-env-env"  // Replace with your actual environment name
+  name                = "Sample-beanstalk-env-env"  
   application         = aws_elastic_beanstalk_application.my_app.name
-  solution_stack_name = "Python 3.11 running on 64bit Amazon Linux 2023/4.1.3"  // Ensure this matches your manual setup
-  version_label       = aws_elastic_beanstalk_application_version.my_app_version.version_label
+  solution_stack_name = "Python 3.11 running on 64bit Amazon Linux 2023/4.1.3"  
+  version_label       = aws_elastic_beanstalk_application_version.my_app_version.name
 }
