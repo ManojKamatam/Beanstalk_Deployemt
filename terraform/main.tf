a9c16ae91ac0f08f963abe72d6c193ed40a3f0f4
Manoj Kamatam <128217010+ManojKamatam@users.noreply.github.com>
2024-08-23 13:15:45 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,10 +14,14 @@ variable "application_name" {
   type = string
 }
 
+variable "version_label" {
+  type = string
+}
+
 resource "aws_elastic_beanstalk_application_version" "my_app_version" {
-  application = var.application_name
-  name        = "my-app-version"
-  description = "My Elastic Beanstalk Application Version"
-  bucket      = var.s3_bucket
-  key         = var.s3_key
+  application   = var.application_name
+  version_label = var.version_label
+  description   = "My Elastic Beanstalk Application Version"
+  bucket        = var.s3_bucket
+  key           = var.s3_key
 }
